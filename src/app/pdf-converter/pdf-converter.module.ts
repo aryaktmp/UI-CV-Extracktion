036c939248f82f1d0ee198116b0faa56dfcc39a9
aryaktmp <aryatiowasesa1306@gmail.com>
2025-03-07 10:52:29 +0700

    fix: fix structur folder

--- src/app/pdf-converter/pdf-converter.module.ts
+++ src/app/pdf-converter/pdf-converter.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { PDFConverterRoutingModule } from './pdf-converter.routing.module';
 import { IndexComponentPDFConverter } from './page/index.component';
-import { httpInterceptorProviders } from './interceptors';
+import { httpInterceptorProviders } from './api/interceptors';
 
 @NgModule({
   imports: [CommonModule, HttpClientModule, PDFConverterRoutingModule],
